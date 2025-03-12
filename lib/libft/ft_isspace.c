cffb33bcc8bd434fc9664cd5024f9c848c7ec705
nataiuca <movil.nataiuca@gmail.com>
2025-03-12 18:30:16 +0100

    Add files via upload

---
+++ lib/libft/ft_isspace.c
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_isspace.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jgamarra <jgamarra@student.42madrid.com>   +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/01/14 16:03:22 by jgamarra          #+#    #+#             */
+/*   Updated: 2025/01/26 16:01:05 by jgamarra         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+/*
+* Check if the character is a space
+* Return:
+* 1 if is a space 0 if not
+*/
+int	ft_isspace(char c)
+{
+	if (c == ' ' || c == '\t' || c == '\n' || c == '\v'
+		|| c == '\f' || c == '\r')
+	{
+		return (1);
+	}
+	return (0);
+}
