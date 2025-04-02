386c7091fd4b615e57083468a8757931c5608a20
nataiuca <movil.nataiuca@gmail.com>
2025-04-02 21:08:41 +0200

    Add files via upload

--- pipe.c
+++ pipe.c
@@ -6,7 +6,7 @@
 /*   By: natferna <natferna@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/12 21:31:57 by jgamarra          #+#    #+#             */
-/*   Updated: 2025/03/31 23:01:16 by natferna         ###   ########.fr       */
+/*   Updated: 2025/04/02 21:04:48 by natferna         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,16 +26,37 @@ struct cmd* pipecmd(struct cmd *left, struct cmd *right)
 
 struct cmd* parsepipe(char **ps, char *es)
 {
-  struct cmd *cmd;
-  t_minishell	minishell;
+    struct cmd *cmd;
+    t_minishell minishell;
 
-  // printf("parsepipe->ps: %s\n", *ps);
-  cmd = parseexec(ps, es, &minishell);
-  if(peek(ps, es, "|")){
-    gettoken(ps, es, 0, 0);
-    cmd = pipecmd(cmd, parsepipe(ps, es));
-  }
-  return cmd;
+    // Parsea el comando de la izquierda
+    cmd = parseexec(ps, es, &minishell);
+	if (((struct execcmd *)cmd)->argv[0] == NULL) {
+        printf("parse error near `|'\n");
+        exit(1);
+    }
+
+    // Si se encuentra un pipe, procesamos el comando de la derecha
+    if (peek(ps, es, "|")) {
+        // Consumir el token pipe
+        gettoken(ps, es, 0, 0);
+
+        // Verificar que después del pipe exista un comando válido.
+        // Si se encuentra otro '|' o se llegó al final de la entrada, es un error.
+        if (!**ps || peek(ps, es, "|")) {
+            printf("parse error near `|'\n");
+            exit(1);
+        }
+
+        // Parsea el comando de la derecha (recursivamente, lo que permite múltiples pipes)
+        struct cmd *right = parsepipe(ps, es);
+        if (right == NULL) {
+            printf("parse error near `|'\n");
+            exit(1);
+        }
+        cmd = pipecmd(cmd, right);
+    }
+    return cmd;
 }
 
 struct cmd* parseline(char **ps, char *es)
